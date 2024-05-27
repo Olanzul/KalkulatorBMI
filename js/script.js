419b9f181c2efc99c90b08fb8136baed62e6a5a8
Olanzul <olanzul@gmail.com>
2024-05-27 09:14:51 +0700

    last touch

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
+const container = document.querySelector('.container');
+
 function calculateBMI(){
     const height = document.getElementById('height').value/100;
     const weight = document.getElementById('weight').value;
-
+    
+    
     if (height > 0 && weight > 0){
         const bmi = (weight / (height * height)).toFixed(2);
         let category;
-        console.log(bmi);
 
         if (bmi < 18.5) {
-            category = 'Kurus';
+            category = 'Thin';
         } else if (bmi >= 18.5 && bmi <24.9) {
-            category = 'Normal';
+            category = 'Ideal';
         } else if (bmi >= 25 && bmi < 29.9) {
-            category = 'Gemuk';
+            category = 'Fat';
         } else {
-            category = 'Obesitas';
+            category = 'Obesse';
         }
-        // document.getElementById('category').innerHTML = category;
-        document.getElementById('result').innerHTML = 'Your BMI is : ' + bmi + category; 
+        document.getElementById('result').innerHTML = 'Your BMI is : ' + bmi + ' (' + category + ')'; 
     } else {
         document.getElementById('result').innerHTML = 'Input required.';
     }
+    container.style.height = '395px';
+    return;
 }
 
 function resetForm() {
     document.getElementById('form').reset();
     document.getElementById('result').innerHTML = '';
+    container.style.height = '240' ;
+    return;
 }
